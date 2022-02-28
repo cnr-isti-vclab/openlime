34abb98b37c25ae78bcf683a6b1303cd8320ed8e
Fabio Bettio <fabio@crs4.it>
2022-02-28 15:21:23 +0100

    New release notes drafter (via github action) -  Bug fixed

--- jsdoc/releases/gen_changelog.sh
+++ jsdoc/releases/gen_changelog.sh
@@ -7,14 +7,14 @@ done
 
 if [[ "${#a[@]}" -gt 0 ]] 
 then
-    echo "### Tag ${a[1]}"
-    git tag -l --format='%(contents)' "${a[1]}"
+    echo "### Tag ${a[0]}"
+    git tag -l --format='%(contents)' "${a[0]}"
 fi
 
 if [[ "${#a[@]}" -gt 1 ]] 
 then
-    CURR="${a[1]}"
-    PREV="${a[2]}"
+    CURR="${a[0]}"
+    PREV="${a[1]}"
     echo "### Commits" 
     git log --pretty=format:"- %s (%ci %cn)" ${PREV}...${CURR}
 fi
