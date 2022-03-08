e1712c2739b121352be285463414c18ebe447cc0
Fabio Bettio <fabio@crs4.it>
2022-03-08 12:25:15 +0100

    Prepare release v1.0.0 - Add Release notes

--- jsdoc/releases/gen_changelog.sh
+++ jsdoc/releases/gen_changelog.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-s=$(git tag --sort -version:refname)
+s=$(git tag -l --sort=version:refname v*)
 readarray -t a <<<"$s"
 for i in "${!a[@]}" ; do 
   ! [[ ${a[$i]} =~ ^v[0-9]+\. ]] && unset -v 'a[$i]'
