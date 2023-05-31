b337e28d38a8fc631bed7e419adfc368e61298bd
Fabio Bettio <fabio@crs4.it>
2023-05-31 13:45:38 +0200

    Fix typos

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ import { LayerAnnotation } from './LayerAnnotation.js'
 import { LayerSvgAnnotation } from './LayerSvgAnnotation.js'
 import { EditorSvgAnnotation } from './EditorSvgAnnotation.js'
 import { LayerRTI } from './LayerRTI.js'
-import { LayoutTiles} from './LayoutTiles.js'
 import { LayerNeuralRTI } from './LayerNeuralRTI.js'
 
 let lime = new Viewer('.openlime', { background: 'black', canvas: { preserveDrawingBuffer: true} });
