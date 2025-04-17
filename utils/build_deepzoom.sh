7af151bbaf0c07e717821f0ecf9a43a1516b209e
Fabio Bettio <fabio@crs4.it>
2025-04-17 10:19:14 +0200

    Improve multispectral example style

--- utils/build_deepzoom.sh
+++ utils/build_deepzoom.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for i in *.jpg; do 
 	rm -rf ${i%.jpg}_files;
-	rm -rf ${i%.jpg};
+	rm -rf ${i%.dzi};
 	vips dzsave $i ${i%.jpg} --layout dz --tile-size 256 --overlap 0	  --depth onetile --suffix .jpg[Q=95]; 
 done
