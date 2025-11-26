9b367545a755b72d823f8df279b9dd30261cf3fb
Fabio Bettio <fabio@crs4.it>
2025-11-26 07:22:17 +0100

    remove pointerout event

--- src/LightSphereController.js
+++ src/LightSphereController.js
@@ -109,10 +109,6 @@ class LightSphereController {
             }
         });
 
-        this.dlCanvas.addEventListener("pointerout", (e) => {
-            this.pointerDown = false;
-        });
-
     }
 
     /**
